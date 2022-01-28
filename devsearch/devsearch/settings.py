b8340f87107f9cfe9822d6c865b3d9a65730bd2d
Pawan <pawan.singh.co19@nsut.ac.in>
2022-01-28 20:22:47 +0530

    template integration

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4628=324o^0qb9g^-s83!ryq@0bvkf&b=fszo1a!yom&3-+9jg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +130,7 @@ STATICFILES_DIRS =[
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
