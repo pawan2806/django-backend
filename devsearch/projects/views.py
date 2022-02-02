0e16313900702fbaaabe4e994fad1772a6b6ebca
Pawan <pawan.singh.co19@nsut.ac.in>
2022-02-02 19:54:11 +0530

    added login func

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
+from django.contrib.auth.decorators import login_required
+
 from .forms import ProjectForm
 
 def projects(request):
@@ -16,6 +18,7 @@ def project(request, pk):
     tags= projectObj.tags.all()
     return render(request, 'projects/single-project.html', {'project':projectObj, 'tags':tags})
 
+@login_required(login_url="login")
 def createProject(request):
     form=ProjectForm()
 
@@ -27,7 +30,7 @@ def createProject(request):
     context ={'form' :form}
     return render(request, "projects/project_form.html", context)
 
-
+@login_required(login_url="login")
 def updateProject(request, pk):
     project=Project.objects.get(id=pk)
     form=ProjectForm(instance=project)
@@ -40,6 +43,8 @@ def updateProject(request, pk):
     context ={'form' :form}
     return render(request, "projects/project_form.html", context)
 
+
+@login_required(login_url="login")
 def deleteProject(request, pk):
     project=Project.objects.get(id=pk)
     if request.method=="POST":
