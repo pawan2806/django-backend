42da5c65aceeb0727036b52b485286d00298a74b
Pawan <pawan.singh.co19@nsut.ac.in>
2022-02-02 18:26:29 +0530

    signals integrated

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 import uuid
 import projects.models
 
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name =  models.CharField(max_length=200, blank=True, null=True)
@@ -35,5 +38,3 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
-
-   
